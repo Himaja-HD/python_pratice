5e235f0c665a98af2751ac5f3ee949818d6d93eb
HDC <118044474+Himaja-HD@users.noreply.github.com>
2024-05-16 16:02:25 +0530

    Add files via upload

---
+++ day_1.py
@@ -0,0 +1,24 @@
+"""
+1. print(8,5,2023,sep='/')  -- >      (b)
+2. vaild variable name      -- >      _my_string , try
+3. x=3.5  print(type(x))   -- >      float
+4. int()
+5. end
+6. Boolean
+7. True
+8. False
+9. False
+
+11. l=100
+    b=100
+    area=l*b
+    print("Area of a Rectangle :",area)
+
+12. celsius=28
+    fahrenheit=(celsius*9/5)+32
+    print("Converted Celsius to Fahrenheit",int(fahrenheit),"F")
+
+"""
+
+
+
